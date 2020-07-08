810a53d12914a2d608dd6a7353a1acf6c23c541e
witwiki <witwiki@users.noreply.github.com>
2020-07-08 15:46:26 +1200

    tuesday work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProtocolCardComponent from './components/ProtocolCardComponent/ProtocolCardComponent';
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <br/>
+        <ProtocolCardComponent />
       </header>
+
     </div>
   );
 }
